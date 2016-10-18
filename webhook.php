7eaaf7389809f4bbe894d206eaa178d3cd7f958b
Murilo Sardinha <murilosardinha@gmail.com>
2016-10-18 17:11:30 -0300

    adds logs

--- webhook.php
+++ webhook.php
@@ -2,8 +2,12 @@
   require("functions.php");
 
   $array = $_REQUEST;
+  error_log(print_r($array, TRUE));
 
   $arrlength = count($array);
+  error_log(print_r($arrlength, TRUE));
+  error_log(print_r($array[0], TRUE));
+  error_log(print_r($array[0].email, TRUE));
 
   for($i = 0; $i < $arrlength; $i++) {
     if ($array[$i].event == 'delivered'){
