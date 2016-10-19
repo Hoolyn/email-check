1b33ffe13d82f55920dcd3b9745c652f5c09bdcd
Murilo Sardinha <murilosardinha@gmail.com>
2016-10-19 14:50:15 -0300

    fixes webhook

--- webhook.php
+++ webhook.php
@@ -1,24 +1,18 @@
 <?php
   require("functions.php");
 
-  $array = $HTTP_RAW_POST_DATA;
-  error_log(print_r("array => ", TRUE));
-  error_log(print_r($array, TRUE));
+  $data = json_decode($HTTP_RAW_POST_DATA);
 
-  $hash = $array[0];
-  error_log(print_r("hash => ", TRUE));
-  error_log(print_r($hash, TRUE));
+  foreach ( $data as $obj ){
+    error_log(print_r($obj->email, TRUE));
+    error_log(print_r($obj->event, TRUE));
 
-  $arrlength = count($hash);
-  error_log(print_r("count => ", TRUE));
-  error_log(print_r($arrlength, TRUE));
-
-  for($i = 0; $i < $arrlength; $i++) {
-    if ($hash[$i]["event"] == 'delivered'){
-      insertTable("Lists", $hash[$i]["email"]);
+    if ($obj->event == 'delivered'){
+      insertTable("Lists", $obj->email);
     }
-    if (($hash[$i]["event"] == 'dropped') || ($hash[$i]["event"] == 'bounce') || ($hash[$i]["event"] == 'spamreport')){
-      insertTable("ColdLists", $hash[$i]["email"]);
+    if (($obj->event == 'dropped') || ($obj->event == 'bounce') || ($obj->event == 'spamreport')){
+      insertTable("ColdLists", $obj->email);
     }
+
   }
 ?>
