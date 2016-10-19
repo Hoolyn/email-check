f6d97278dd40ae87bf936ae8ae4eec41b7f81fc4
Murilo Sardinha <murilosardinha@gmail.com>
2016-10-19 14:37:29 -0300

    fixes logs

--- webhook.php
+++ webhook.php
@@ -5,20 +5,20 @@
   error_log(print_r("array => ", TRUE));
   error_log(print_r($array, TRUE));
 
-  $arrlength = count($array);
+  $hash = $array[0];
+  error_log(print_r("hash => ", TRUE));
+  error_log(print_r($hash, TRUE));
+
+  $arrlength = count($hash);
   error_log(print_r("count => ", TRUE));
   error_log(print_r($arrlength, TRUE));
 
-  error_log(print_r("email => ", TRUE));
-  error_log(print_r($array["email"], TRUE));
-  error_log(print_r($array[0]["email"], TRUE));
-
   for($i = 0; $i < $arrlength; $i++) {
-    if ($array[$i]["event"] == 'delivered'){
-      insertTable("Lists", $array[$i]["email"]);
+    if ($hash[$i]["event"] == 'delivered'){
+      insertTable("Lists", $hash[$i]["email"]);
     }
-    if (($array[$i]["event"] == 'dropped') || ($array[$i]["event"] == 'bounce') || ($array[$i]["event"] == 'spamreport')){
-      insertTable("ColdLists", $array[$i]["email"]);
+    if (($hash[$i]["event"] == 'dropped') || ($hash[$i]["event"] == 'bounce') || ($hash[$i]["event"] == 'spamreport')){
+      insertTable("ColdLists", $hash[$i]["email"]);
     }
   }
 ?>
